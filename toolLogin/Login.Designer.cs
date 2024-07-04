83b7e0d5125748db899fdffce0e8d02300b4ab7b
TrungHieuMobile <hoangtrunghieu26112002@gmail.com>
2024-07-04 17:05:32 +0700

    auto login

---
+++ toolLogin/Login.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace toolLogin
+{
+	partial class Login
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.txtEmail = new System.Windows.Forms.TextBox();
+			this.txtPassword = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(192, 165);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(89, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Enter";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// txtEmail
+			// 
+			this.txtEmail.Location = new System.Drawing.Point(86, 69);
+			this.txtEmail.Name = "txtEmail";
+			this.txtEmail.Size = new System.Drawing.Size(310, 22);
+			this.txtEmail.TabIndex = 1;
+			// 
+			// txtPassword
+			// 
+			this.txtPassword.Location = new System.Drawing.Point(86, 115);
+			this.txtPassword.Name = "txtPassword";
+			this.txtPassword.Size = new System.Drawing.Size(310, 22);
+			this.txtPassword.TabIndex = 2;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(481, 245);
+			this.Controls.Add(this.txtPassword);
+			this.Controls.Add(this.txtEmail);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox txtEmail;
+		private System.Windows.Forms.TextBox txtPassword;
+	}
+}
+
---
+++ toolLogin/Login.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using System.Linq;
+
+namespace toolLogin
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private void RunAdbCommand(string command)
+        {
+            ProcessStartInfo procStartInfo = new ProcessStartInfo("cmd", "/c " + command)
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            Process proc = new Process { StartInfo = procStartInfo };
+            proc.Start();
+            string result = proc.StandardOutput.ReadToEnd();
+            Console.Write(result);
+            //MessageBox.Show(result);
+        }
+        private (int x, int y) GetElementCoordinates(string elementText)
+        {
+            // Lấy thông tin layout của màn hình hiện tại
+            RunAdbCommand("adb shell uiautomator dump /sdcard/window_dump.xml");
+            RunAdbCommand("adb pull /sdcard/window_dump.xml");
+
+            // Phân tích file XML để tìm tọa độ phần tử cụ thể
+            string xmlContent = File.ReadAllText("window_dump.xml");
+            var doc = XDocument.Parse(xmlContent);
+            var element = doc.Descendants("node")
+                             .FirstOrDefault(node => node.Attribute("text")?.Value == elementText);
+
+            if (element != null)
+            {
+                var bounds = element.Attribute("bounds")?.Value;
+                var match = Regex.Match(bounds, @"\[(\d+),(\d+)\]\[(\d+),(\d+)\]");
+                if (match.Success)
+                {
+                    int x1 = int.Parse(match.Groups[1].Value);
+                    int y1 = int.Parse(match.Groups[2].Value);
+                    int x2 = int.Parse(match.Groups[3].Value);
+                    int y2 = int.Parse(match.Groups[4].Value);
+                    int x = (x1 + x2) / 2;
+                    int y = (y1 + y2) / 2;
+                    return (x, y);
+                }
+            }
+            return (0, 0);
+        }
+        private (int x, int y) WaitForCoordinates(string elementText, int timeout = 30000)
+        {
+            int elapsed = 0;
+            int interval = 1000;
+
+            while (elapsed < timeout)
+            {
+                var coordinates = GetElementCoordinates(elementText);
+                if (coordinates.x != 0 && coordinates.y != 0)
+                {
+                    return coordinates;
+                }
+
+                System.Threading.Thread.Sleep(interval);
+                elapsed += interval;
+            }
+
+            throw new Exception($"Timeout: Element with text '{elementText}' not found.");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string email = txtEmail.Text;
+            string password = txtPassword.Text;
+            RunAdbCommand("adb shell am start -a android.settings.ADD_ACCOUNT_SETTINGS");
+            var (a, b) = WaitForCoordinates("Google");
+            if (a != 0 && b != 0)
+            {
+                System.Threading.Thread.Sleep(1000);
+                RunAdbCommand($"adb shell input tap 500 800");
+
+                System.Threading.Thread.Sleep(1000);
+                RunAdbCommand($"adb shell input text \"{email}\"");
+                RunAdbCommand("adb shell input keyevent 66");
+                System.Threading.Thread.Sleep(1000);
+                RunAdbCommand($"adb shell input text \"{password}\"");
+                RunAdbCommand("adb shell input keyevent 66");
+                System.Threading.Thread.Sleep(1000);
+                (int x, int y) = GetElementCoordinates("I agree");
+                if (x != 0 && y != 0)
+                {
+                    RunAdbCommand($"adb shell input tap {x} {y}");
+                }
+                else
+                {
+                    MessageBox.Show("Không tìm thấy nút 'Next'");
+                }
+            }
+        }
+	} 
+}
